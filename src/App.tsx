063ce9a3df9936b480c4e192d9f66049cae83139
Yasin Khan <yasinkkhan@gmail.com>
2023-07-14 16:17:36 -0400

    Added TS types to pieces of state

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,8 @@ import { useState } from 'react';
 import SportSelector from './components/SportSelector';
 
 function App() {
-  const [sportsList] = useState(['NBA', 'EPL', 'NHL', 'MLB', 'NFL']);
-  const [selectedSport, setSelectedSport] = useState('');
+  const [sportsList] = useState<string[]>(['NBA', 'EPL', 'NHL', 'MLB', 'NFL']);
+  const [selectedSport, setSelectedSport] = useState<string>('');
 
   return (
     <div className="max-w-screen-sm container mx-auto px-4 box-border border-2 border-sky-500 min-h-screen">
