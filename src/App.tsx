aceb054f6004f35ff4dde8c0c891e03b31d44108
Yasin Khan <yasinkkhan@gmail.com>
2023-07-13 22:16:11 -0400

    Added some additional styles to the App component

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import SportSelector from './components/SportSelector';
 
 function App() {
   return (
-    <div className="max-w-screen-sm container mx-auto">
+    <div className="max-w-screen-sm container mx-auto px-4 box-border border-2 border-sky-500 min-h-screen">
       <SportSelector />
     </div>
   );
