dd038e6d9a13492fbad8170decd85f3df8ba5dbd
Yasin Khan <yasinkkhan@gmail.com>
2023-07-13 22:11:22 -0400

    Restricted the max-width of App component to mobile screen

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,9 @@ import SportSelector from './components/SportSelector';
 
 function App() {
   return (
-    <>
+    <div className="max-w-screen-sm container mx-auto">
       <SportSelector />
-    </>
+    </div>
   );
 }
 
