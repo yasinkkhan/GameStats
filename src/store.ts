eb721d41ed8320d7ee4d74a07e33fc1654f812af
Yasin Khan <yasinkkhan@gmail.com>
2023-07-15 14:44:19 -0400

    Added a store for react-redux RTK

---
+++ src/store.ts
@@ -0,0 +1,16 @@
+import { configureStore } from '@reduxjs/toolkit';
+// ...
+
+export const store = configureStore({
+  reducer: {
+    sports: sportsReducer,
+    // seasons: seasonsReducer,
+    // games: gamesReducer,
+    // gamesDetails: gamesDetailsReducer,
+  },
+});
+
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<typeof store.getState>;
+// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+export type AppDispatch = typeof store.dispatch;
