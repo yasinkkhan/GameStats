dec1b8e3e172ba882b5cc129c3349dd42804cecc
Yasin Khan <yasinkkhan@gmail.com>
2023-07-16 13:38:38 -0400

    Added button component from ShadCN

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import './App.css';
-
+import { useAppSelector } from '@/hooks';
 import SportSelector from './components/SportSelector';
 
 function App() {
+  const selectedSport = useAppSelector((state) => {
+    return state.sports.selectedSport;
+  });
+
   return (
     <div className="max-w-screen-sm container mx-auto px-4 box-border border-2 border-sky-500 min-h-screen">
-      <SportSelector />
+      {!selectedSport && <SportSelector />}
     </div>
   );
 }
