e58a6a67d1c916c90d4a1af3f396c553827d83b5
Yasin Khan <yasinkkhan@gmail.com>
2023-07-18 19:32:02 -0400

    Added support for custom headers required by the NPA API

--- src/store/apis/nbaApiSlice.ts
+++ src/store/apis/nbaApiSlice.ts
@@ -3,7 +3,14 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const nbaApi = createApi({
   reducerPath: 'nbpaApi',
-  baseQuery: fetchBaseQuery({ baseUrl: nbaApiSecrets['baseUrl'] }),
+  baseQuery: fetchBaseQuery({
+    baseUrl: nbaApiSecrets['baseUrl'],
+    prepareHeaders: (headers) => {
+      headers.set('X-RapidAPI-Key', nbaApiSecrets['X-RapidAPI-Key']);
+      headers.set('X-RapidAPI-Host', nbaApiSecrets['X-RapidAPI-Host']);
+      return headers;
+    },
+  }),
   endpoints: (builder) => ({
     getAllNbaSeason: builder.query({
       query: () => 'Hello',
