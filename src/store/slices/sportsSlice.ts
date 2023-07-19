743de793ae84a670443893871d3843fd087eb345
Yasin Khan <yasinkkhan@gmail.com>
2023-07-18 22:25:27 -0400

    imported hook from nbaApiSlice into sportsSlice

--- src/store/slices/sportsSlice.ts
+++ src/store/slices/sportsSlice.ts
@@ -1,5 +1,6 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import type { RootState } from '../../store';
+import { useGetAllNbaSeasonsQuery } from '@/store/apis/nbaApiSlice';
 
 // Define a type for the slice state
 interface SportsState {
