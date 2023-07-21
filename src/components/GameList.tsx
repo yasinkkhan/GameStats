d4dba8824cb1ba229049980aab93f68fda01e5c1
Yasin Khan <yasinkkhan@gmail.com>
2023-07-20 20:10:14 -0400

    Use skip property of react hook to only call api when selectedSeason has been set

--- src/components/GameList.tsx
+++ src/components/GameList.tsx
@@ -1,13 +1,12 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useAppSelector, useAppDispatch } from '@/hooks';
 import { useGetAllGamesForSeasonQuery } from '@/store/apis/nbaApiSlice';
 
 const GameList: React.FC = (): JSX.Element => {
   const selectedSeason = useAppSelector((state) => state.sports.selectedSeason);
-
-  if (selectedSeason) {
-    const { data, error, isLoading } = useGetAllGamesForSeasonQuery(selectedSeason);
-  }
+  const { data, error, isLoading } = useGetAllGamesForSeasonQuery(selectedSeason as number, {
+    skip: !selectedSeason, // Set skip to true when selectedSeason is null or undefined
+  });
 
   return <div>Hello</div>;
 };
