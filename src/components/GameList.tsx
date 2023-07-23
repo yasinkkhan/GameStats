999ab050f721fc62cb73e88c3b523db059604387
Yasin Khan <yasinkkhan@gmail.com>
2023-07-23 18:07:12 -0400

    Implmented the date picker, implementation needs clean up at this point

--- src/components/GameList.tsx
+++ src/components/GameList.tsx
@@ -2,10 +2,16 @@ import React from 'react';
 import { useAppSelector } from '@/hooks';
 import { useGetAllGamesForSeasonQuery } from '@/store/apis/nbaApiSlice';
 
+import DatePicker from './DatePicker';
+
 const GameList: React.FC = (): JSX.Element => {
   const selectedSeason = useAppSelector((state) => state.sports.selectedSeason);
   const seasonFirstGameDate = useAppSelector((state) => state.sports.seasonFirstGameDate);
   const seasonLastGameDate = useAppSelector((state) => state.sports.seasonLastGameDate);
+
+  // TO DO
+  // I don't like that I'm doing this here, I think doing this in the slices would be better.
+  // (MAY BE)
   const { data, error, isLoading } = useGetAllGamesForSeasonQuery(selectedSeason as number, {
     skip: !selectedSeason, // Set skip to true when selectedSeason is null or undefined
   });
@@ -32,10 +38,24 @@ const GameList: React.FC = (): JSX.Element => {
 
   let renderCalendarComponent;
   if (seasonFirstGameDate != '' && seasonLastGameDate != '') {
-    // Render calendar
+    const seasonFirstGameDateObject = new Date(seasonFirstGameDate);
+    const seasonLastGameDateObject = new Date(seasonLastGameDate);
+
+    renderCalendarComponent = (
+      <DatePicker
+        firstSelectableDate={seasonFirstGameDateObject}
+        lastSelectableDate={seasonLastGameDateObject}
+      />
+    );
   }
 
-  return <div>Hola</div>;
+  return (
+    <div>
+      <p>The first selectable date is: {seasonFirstGameDate}</p>
+      <p>The last selectable date is: {seasonFirstGameDate}</p>
+      {renderCalendarComponent}
+    </div>
+  );
 };
 
 export default GameList;
