709bd680c5fd78a92070138e35e6370349804f34
Yasin Khan <yasinkkhan@gmail.com>
2023-07-23 17:54:12 -0400

    Updated DatePicker component to have a min and max selectable dates

--- src/components/GameList.tsx
+++ src/components/GameList.tsx
@@ -4,31 +4,38 @@ import { useGetAllGamesForSeasonQuery } from '@/store/apis/nbaApiSlice';
 
 const GameList: React.FC = (): JSX.Element => {
   const selectedSeason = useAppSelector((state) => state.sports.selectedSeason);
+  const seasonFirstGameDate = useAppSelector((state) => state.sports.seasonFirstGameDate);
+  const seasonLastGameDate = useAppSelector((state) => state.sports.seasonLastGameDate);
   const { data, error, isLoading } = useGetAllGamesForSeasonQuery(selectedSeason as number, {
     skip: !selectedSeason, // Set skip to true when selectedSeason is null or undefined
   });
 
-  let renderedContent;
-  if (isLoading) {
-    renderedContent = <p>Loading...</p>;
-  } else if (error) {
-    // TO DO
-    // You need to confirm what type error actually is
-    // and handle it more gracefully
-    renderedContent = <p>{error.toString()}</p>;
-  } else if (data) {
-    // TO DO
-    // I'm currently handling populating the first game in the season
-    // and the last game in the season directly in this component
-    // to populate the selectable dates in the calendar
-    // I want to do this in my reducers instead
-    // console.log(data);
-    renderedContent = <p>Got data</p>;
-  } else {
-    renderedContent = <p>Something weird happened</p>;
+  // let renderedContent;
+  // if (isLoading) {
+  //   renderedContent = <p>Loading...</p>;
+  // } else if (error) {
+  //   // TO DO
+  //   // You need to confirm what type error actually is
+  //   // and handle it more gracefully
+  //   renderedContent = <p>{error.toString()}</p>;
+  // } else if (data) {
+  //   // TO DO
+  //   // I'm currently handling populating the first game in the season
+  //   // and the last game in the season directly in this component
+  //   // to populate the selectable dates in the calendar
+  //   // I want to do this in my reducers instead
+  //   // console.log(data);
+  //   renderedContent = <p>Got data</p>;
+  // } else {
+  //   renderedContent = <p>Something weird happened</p>;
+  // }
+
+  let renderCalendarComponent;
+  if (seasonFirstGameDate != '' && seasonLastGameDate != '') {
+    // Render calendar
   }
 
-  return <div>{renderedContent}</div>;
+  return <div>Hola</div>;
 };
 
 export default GameList;
