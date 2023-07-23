4f3338cdc2935c9b1a09c412f29070511a97dc6b
Yasin Khan <yasinkkhan@gmail.com>
2023-07-23 18:59:00 -0400

    Making a commit here because I'm trying to figure out how to handle the DatePicker component when a date is selected but running into some trouble

--- src/components/DatePicker.tsx
+++ src/components/DatePicker.tsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import { useAppSelector, useAppDispatch } from '@/hooks';
+import { setSelectedDate } from '@/store/slices/sportsSlice';
+
 import { format } from 'date-fns';
 import { Calendar as CalendarIcon } from 'lucide-react';
 
@@ -15,31 +18,55 @@ interface DatePickerProps {
 const DatePicker: React.FC<DatePickerProps> = ({ firstSelectableDate, lastSelectableDate }): JSX.Element => {
   const [date, setDate] = React.useState<Date>();
 
-  const handleSetSelectedDate = () => {
-    return '';
+  const dispatch = useAppDispatch();
+
+  const setSelectedDateHandler = (selectedDate: Date) => {
+    dispatch(setSelectedDate(selectedDate));
   };
+
+  if (date) {
+    setSelectedDateHandler(date);
+  }
+
+  const selectedDate = useAppSelector((state) => {
+    return state.sports.selectedDate;
+  });
 
   return (
     <Popover>
       <PopoverTrigger asChild>
         <Button
           variant={'outline'}
-          className={cn('w-[280px] justify-start text-left font-normal', !date && 'text-muted-foreground')}
+          className={cn(
+            'w-[280px] justify-start text-left font-normal',
+            !selectedDate && 'text-muted-foreground'
+          )}
         >
           <CalendarIcon className="mr-2 h-4 w-4" />
-          {date ? format(date, 'PPP') : <span>Pick a date</span>}
+          {selectedDate ? format(selectedDate, 'PPP') : <span>Pick a date</span>}
         </Button>
       </PopoverTrigger>
       <PopoverContent className="w-auto p-0">
-        <Calendar
-          mode="single"
-          defaultMonth={firstSelectableDate}
-          fromDate={firstSelectableDate}
-          toDate={lastSelectableDate}
-          selected={date}
-          onSelect={handleSetSelectedDate}
-          initialFocus
-        />
+        {/* Use ternary operator to conditionally set the selected and onSelect props */}
+        {selectedDate !== null ? (
+          <Calendar
+            mode="single"
+            defaultMonth={firstSelectableDate}
+            fromDate={firstSelectableDate}
+            toDate={lastSelectableDate}
+            selected={date}
+            onSelect={setDate}
+            initialFocus
+          />
+        ) : (
+          <Calendar
+            mode="single"
+            defaultMonth={firstSelectableDate}
+            fromDate={firstSelectableDate}
+            toDate={lastSelectableDate}
+            initialFocus
+          />
+        )}
       </PopoverContent>
     </Popover>
   );
