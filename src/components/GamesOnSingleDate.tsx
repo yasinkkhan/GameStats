5a78c84b52959ff8e29ee65da1c348794267591c
Yasin Khan <yasinkkhan@gmail.com>
2023-07-23 22:18:43 -0400

    Stopping here for now hence commit

--- src/components/GamesOnSingleDate.tsx
+++ src/components/GamesOnSingleDate.tsx
@@ -2,10 +2,26 @@ import React from 'react';
 import { useAppSelector } from '@/hooks';
 
 const GamesOnSingleDate: React.FC = (): JSX.Element => {
+  const gamesOnSelectedDate = useAppSelector((state) => state.sports.gamesOnSelectedDate);
+
+  const renderedGames = gamesOnSelectedDate.map((game) => {
+    console.log(game);
+
+    return <table></table>;
+  });
+
   return (
     <>
       <div>
-        <p>GamesOnSingleDate</p>
+        <table>
+          <th>
+            <tr></tr>
+          </th>
+          <td>
+            <tr></tr>
+          </td>
+        </table>
+        {renderedGames}
       </div>
     </>
   );
